26e736ed28e949fe5abad25445159750dfc3be89
christian.staeblein <christian.staeblein@gmail.com>
2026-09-25 22:43:46 +0100

    question now via command line

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from openai import OpenAI
 
 
-def main():
-    role = "user"
-    content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
+def main():
+
+    #getting command line arguments
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+
+    role = "user"
+    content = args.user_prompt
+
+    #Getting environmental parameters
     load_dotenv()
     api_key = os.environ.get("OPENROUTER_API_KEY")
 
     if api_key is None:
         raise RuntimeError("No API key found")
 
+
+    #Initialising OpenRouter AI
     client = OpenAI(
         base_url="https://openrouter.ai/api/v1",
         api_key=api_key,
     )
 
+    #Sending prompt
     response = client.chat.completions.create(
         model="openrouter/free",
         messages=[
@@ -28,11 +41,13 @@ def main():
         ],
     )
 
-    print(f"\n{role} prompt: {content}")
-    print("\n------")
+
+    #Printing prompt response and usage to console
     print(f"Prompt tokens: {response.usage.prompt_tokens}")
     print(f"Response tokens: {response.usage.completion_tokens}")
-    print("------\n")
+    print("\n------")
+    print(f"\n{role} prompt: {content}")
+    print("\n------\n")
     print(response.choices[0].message.content)
 
 
